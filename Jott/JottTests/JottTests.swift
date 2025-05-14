6d92c91996615a85151069b312f0b53b716068ca
cshireman <chris@shireman.net>
2025-05-14 09:52:01 -0700

    Initial Commit

---
+++ Jott/JottTests/JottTests.swift
@@ -0,0 +1,16 @@
+//
+//  JottTests.swift
+//  JottTests
+//
+//  Created by Chris Shireman on 5/14/25.
+//
+
+import Testing
+
+struct JottTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Jott/JottUITests/JottUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  JottUITestsLaunchTests.swift
+//  JottUITests
+//
+//  Created by Chris Shireman on 5/14/25.
+//
+
+import XCTest
+
+final class JottUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
